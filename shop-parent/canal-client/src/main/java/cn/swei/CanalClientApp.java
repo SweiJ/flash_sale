05b1707c6ad90344f1ac400e0cf50fd4c48dae9f
sweiJ <Swei.J@outlook.com>
2022-12-06 20:18:21 +0800

    canal-client

---
+++ shop-parent/canal-client/src/main/java/cn/swei/CanalClientApp.java
@@ -0,0 +1,14 @@
+package cn.swei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by lanxw
+ */
+@SpringBootApplication
+public class CanalClientApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CanalClientApp.class,args);
+    }
+}
